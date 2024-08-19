37cfdd15650caaf8dcb45f841ac998d4345f1649
personalPanda <rishistd931@gmail.com>
2024-08-19 23:57:08 +0530

    new commit

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
@@ -49,7 +49,7 @@ store.on("error", () => {
 
 const sessionOptions = {
   store,
-  secret: "mysupersecretcode",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
