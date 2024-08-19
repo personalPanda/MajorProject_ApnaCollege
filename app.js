0ece0e950880c8e5a5fbb44846c475ceeeb6b201
personalPanda <rishistd931@gmail.com>
2024-08-19 23:24:01 +0530

    connet-mongo error solved

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.engine("ejs", engine);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
-  mongUrl: dbUrl,
+  mongoUrl: dbUrl,
   crypto: {
     secret: "mysupersecretcode",
   },
